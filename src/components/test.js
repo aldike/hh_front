8f93c9ed7d1be045ad5a716d8556cd73586fcd36
aldike <aldy_mon@mail.ru>
2023-08-16 17:56:06 +0600

    First lesson done

---
+++ src/components/test.js
@@ -0,0 +1,8 @@
+export default function Test(){
+    const name = "Aldy"
+    return (<div>
+        <h1>My test Component {name}</h1>
+        <p>My test Parag ...</p>
+        <a>test link</a>
+    </div>)
+}
